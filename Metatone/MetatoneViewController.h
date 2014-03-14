fe2334a5d431bb78374ff4dad17edf6fc6de35be
cpmpercussion <cpm@charlesmartin.com.au>
2014-03-14 17:17:42 +1100

    Initial Commit? Probably.

--- Metatone/MetatoneViewController.h
+++ Metatone/MetatoneViewController.h
@@ -15,5 +15,11 @@
 
 
 @interface MetatoneViewController : UIViewController <PdReceiverDelegate, LoopingNoteDelegate,MetatoneNetworkManagerDelegate>
+@property (weak, nonatomic) IBOutlet UISwitch *loopSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *fieldSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *cymbalSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *noteSwitch;
+
+@property (strong, nonatomic) NSString* lastGesture;
 
 @end
--- Metatone/MetatoneViewController.m
+++ Metatone/MetatoneViewController.m
@@ -8,15 +8,30 @@
 
 #import "MetatoneViewController.h"
 #import "MetatoneTouchView.h"
+#import "ScaleMaker.h"
+#include <stdlib.h>
 
 #define TAP_MODE_FIELDS 0
 #define TAP_MODE_MELODY 1
 #define TAP_MODE_BOTH 2
 #define LOOP_TIME 5000
 
-@interface UITouch (Private)
--(float)_pathMajorRadius;
-@end
+#define SCALE_MODE_F_MIXO 0
+#define SCALE_MODE_FSHARP_LYD 1
+#define SCALE_MODE_C_LYDSHARP 2
+#define METATONE_SCALE_MESSAGE @"SCALE"
+
+#define METATONE_SWITCH_MESSAGE @"SWITCH"
+#define METATONE_FIELDS_MESSAGE @"AUTOFIELDS"
+#define METATONE_LOOP_MESSAGE @"LOOP"
+#define METATONE_RESET_MESSAGE @"RESET"
+#define METATONE_TAPMODE_MESSAGE @"TAPMODE"
+
+#define LOOPED_NOTE_LIMIT 200
+
+//@interface UITouch (Private)
+//-(float)_pathMajorRadius;
+//@end
 
 
 @interface MetatoneViewController () {
@@ -28,6 +43,7 @@
 @property (strong, nonatomic) CMMotionManager* motionManager;
 @property (nonatomic) Boolean oscLogging;
 @property (nonatomic) Boolean accelLogging;
+@property (weak, nonatomic) IBOutlet UILabel *scaleLabel;
 
 @property (nonatomic) Boolean tapLooping;
 @property (weak, nonatomic) IBOutlet UILabel *oscLoggingLabel;
@@ -35,10 +51,17 @@
 @property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGestureRecognizer;
 @property (strong, nonatomic) NSMutableArray *loopedNotes;
 @property (weak, nonatomic) IBOutlet MetatoneTouchView *touchView;
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
 @property (nonatomic) int tapMode;
+@property (nonatomic) int scaleMode;
 @end
 
 @implementation MetatoneViewController
+
+- (NSMutableArray *) loopedNotes {
+    if (!_loopedNotes) _loopedNotes = [[NSMutableArray alloc] init];
+    return _loopedNotes;
+}
 
 - (PdAudioController *) audioController
 {
@@ -59,6 +82,8 @@
 }
 
 
+void arraysize_setup();
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -72,7 +97,11 @@
         NSLog(@"audioController initialised.");
     }
     
-    [PdBase openFile:@"metatone_sounds.pd" path:[[NSBundle mainBundle] bundlePath]];
+    
+    // Load externs
+    arraysize_setup();
+    
+    [PdBase openFile:@"birdsnestsounds.pd" path:[[NSBundle mainBundle] bundlePath]];
     [self.audioController setActive: YES];
     [self.audioController print];
     [PdBase setDelegate:self];
@@ -81,6 +110,7 @@
     
     // Setup Logging
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"OSCLogging"]) {
+        self.oscLogging = YES;
         [self setupOscLogging];
         NSLog(@"Setup Logging.");
         
@@ -89,7 +119,11 @@
         }
     } else {
         [self.oscLoggingLabel setText:@""];
-        NSLog(@"Stopped Logging.");
+        self.oscLogging = NO;
+        // Turned off All Network functions when logging is switched off.
+#pragma mark - TODO make sure this works.
+        //[self setupOscLogging];
+        NSLog(@"No OSC Logging.");
     }
     
     
@@ -110,31 +144,52 @@
     }
     
     // Looping Test
-    self.tapLooping = YES;
-    self.tapMode = 0;
+    self.tapLooping = NO;
+    self.tapMode = 1;
+    self.scaleMode = 0;
+    [self.scaleLabel setText:@"F Mixo"];
+    
+    // Setup background image
+    int choice = arc4random_uniform(3);
+    if (choice == 0) {
+        [self.backgroundImage setImage:[UIImage imageNamed:@"forest.jpg"]];
+    } else if (choice == 1) {
+        [self.backgroundImage setImage:[UIImage imageNamed:@"path.jpg"]];
+    } else if (choice == 2) {
+        [self.backgroundImage setImage:[UIImage imageNamed:@"treetops.jpg"]];
+    }
+    
+    [self reset:Nil];
 }
 
 #pragma mark - Note Methods
 
 -(void)triggerTappedNote:(CGPoint)tapPoint {
     // Send to Pd
-    if (self.tapMode == TAP_MODE_FIELDS || self.tapMode == TAP_MODE_FIELDS) {
+    if (self.tapMode == TAP_MODE_FIELDS || self.tapMode == TAP_MODE_BOTH) {
         [PdBase sendBangToReceiver:@"touch" ]; // makes a small sound
         [PdBase sendFloat:[self calculateDistanceFromCenter:tapPoint]/600 toReceiver:@"tapdistance" ];
     }
-    
     if (self.tapMode == TAP_MODE_MELODY || self.tapMode == TAP_MODE_BOTH) {
         [self sendMidiNoteFromPoint:tapPoint withVelocity:40];
     }
 }
 
 -(void)scheduleRecurringTappedNote:(CGPoint)tapPoint {
-    // max 100 notes in the loopedNotes array.
-    if ([self.loopedNotes count] > 100) [self.loopedNotes removeObjectAtIndex:0];
-    
-    // add the looped note.
-    [self.loopedNotes addObject:[[LoopingNote alloc] initWithNotePoint:tapPoint LoopTime:LOOP_TIME andDelegate:self]];
-    //NSLog(@"Added a Looping Note");
+    LoopingNote *note = [[LoopingNote alloc] initWithNotePoint:tapPoint LoopTime:LOOP_TIME andDelegate:self];
+    if ([self.loopedNotes count] > LOOPED_NOTE_LIMIT) {
+        LoopingNote *firstNote = [self.loopedNotes objectAtIndex:0];
+        [firstNote disable];
+        [self.loopedNotes removeObject:firstNote];
+    }
+    [self.loopedNotes addObject:note];
+}
+
+-(void)clearAllLoopedNotes {
+    for (LoopingNote *note in self.loopedNotes) {
+        [note disable];
+    }
+    [self.loopedNotes removeAllObjects];
 }
 
 -(CGFloat)calculateDistanceFromCenter:(CGPoint)touchPoint {
@@ -148,13 +203,10 @@
     if (self.tapLooping) [self.touchView drawNoteCircleAt:notePoint];
 }
 
-
-
 #pragma mark - Touch
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    //NSLog(@"Tap.");
     UITouch *touch = [touches anyObject];
     CGPoint touchPoint = [touch locationInView:self.view];
     CGFloat distance = [self calculateDistanceFromCenter:touchPoint] /600;
@@ -162,15 +214,14 @@
     // Measure Acceleration
     CMDeviceMotion *motion = self.motionManager.deviceMotion;
     int velocity = (int) (ABS(motion.userAcceleration.z * 3000) + 5) % 128;
-  
-    
+
     // Print touch Area.
     //int area = (int) pow(touch._pathMajorRadius, 2)/4;
     //area = area + 10;
     //  NSLog([NSString stringWithFormat:@"Z Accel: %d, Area: %d", velocity,area]);
     
     // Send to Pd - receiver
-    if (self.tapMode == TAP_MODE_FIELDS || self.tapMode == TAP_MODE_FIELDS) {
+    if (self.tapMode == TAP_MODE_FIELDS || self.tapMode == TAP_MODE_BOTH) {
         [PdBase sendBangToReceiver:@"touch" ]; // makes a small sound
         [PdBase sendFloat:distance toReceiver:@"tapdistance" ];
     }
@@ -194,9 +245,16 @@
     // Testing sending a midi message as well.
     int velocity = ((int) 25 + 100 * (point.y / 800));
     velocity = (int) (velocity * 0.2) + (vel * 0.8); // include the tap acceleration measurement.
+    int note = (int) (distance * 35);
     
-    [PdBase sendNoteOn:1 pitch:((int) (30 + 60*distance)) velocity:velocity];
-    
+    if (self.scaleMode == SCALE_MODE_F_MIXO) {
+        note = [ScaleMaker mixolydian:41 withNote:note];
+    } else if (self.scaleMode == SCALE_MODE_FSHARP_LYD) {
+        note = [ScaleMaker lydian:42 withNote:note];
+    } else if (self.scaleMode == SCALE_MODE_C_LYDSHARP) {
+        note = [ScaleMaker lydianSharpFive:36 withNote:note];
+    }
+    [PdBase sendNoteOn:1 pitch:note velocity:velocity]; // send the note to Pd
 }
 
 
@@ -209,12 +267,8 @@
     CGFloat xVelocity = [touch locationInView:self.view].x - [touch previousLocationInView:self.view].x;
     CGFloat yVelocity = [touch locationInView:self.view].y - [touch previousLocationInView:self.view].y;
     CGFloat velocity = sqrt((xVelocity * xVelocity) + (yVelocity * yVelocity));
-    
-    //NSLog([NSString stringWithFormat:@"Velocity: %f", velocity]);
     if (self.oscLogging) [self.networkManager sendMessageWithTouch:[touch locationInView:self.view] Velocity:velocity];
-    
     [self.touchView drawMovingTouchCircleAt:[touch locationInView:self.view]];
-
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
@@ -228,34 +282,22 @@
 // Cluster Auto Play Switch
 - (IBAction)clustersOn:(UISwitch *)sender {
     if (self.oscLogging) [self.networkManager sendMesssageSwitch:@"clustersOn" On:sender.on];
-    if (sender.on)
-    {
-        [PdBase sendFloat:1 toReceiver:@"autoBowl"];
-    } else {
-        [PdBase sendFloat:0 toReceiver:@"autoBowl"];
-    }
+    float value = (sender.on) ? 1 : 0;
+    [PdBase sendFloat:value toReceiver:@"autoBowl"];
 }
 
 // Cymbal Auto Play Switch
 - (IBAction)cymbalsOn:(UISwitch *)sender {
     if (self.oscLogging) [self.networkManager sendMesssageSwitch:@"cymbalsOn" On:sender.on];
-    if (sender.on)
-    {
-        [PdBase sendFloat:1 toReceiver:@"autoCymbal"];
-    } else {
-        [PdBase sendFloat:0 toReceiver:@"autoCymbal"];
-    }
+    float value = (sender.on) ? 1 : 0;
+    [PdBase sendFloat:value toReceiver:@"autoCymbal"];
 }
 
 // Field Recording auto play Switch
 - (IBAction)fieldsOn:(UISwitch *)sender {
     if (self.oscLogging) [self.networkManager sendMesssageSwitch:@"fieldsOn" On:sender.on];
-    if (sender.on)
-    {
-        [PdBase sendFloat:1 toReceiver:@"autoField"];
-    } else {
-        [PdBase sendFloat:0 toReceiver:@"autoField"];
-    }
+    float value = (sender.on) ? 1 : 0;
+    [PdBase sendFloat:value toReceiver:@"autoField"];
 }
 
 
@@ -263,39 +305,64 @@
 - (IBAction)loopingOn:(UISwitch *)sender {
     if (self.oscLogging) [self.networkManager sendMesssageSwitch:@"loopingOn" On:sender.on];
     self.tapLooping = sender.on;
-    if (!sender.on) {
-        [self.loopedNotes removeAllObjects];
-    }
+    if (!sender.on) [self.loopedNotes removeAllObjects];
+    if (sender.on) [self.networkManager sendMetatoneMessage:METATONE_LOOP_MESSAGE withState:@"on"];
 }
 
 // Reset Sounds Button
+//- (IBAction)reset:(id)sender {
 - (IBAction)reset:(id)sender {
     if (self.oscLogging) [self.networkManager sendMesssageSwitch:@"resetButton" On:YES];
     [PdBase sendBangToReceiver:@"randomiseSounds"];
-    self.tapMode = (self.tapMode + 1) % 3;
+    self.tapMode = 1 + ((self.tapMode + 1) % 2);
+    //NSLog([NSString stringWithFormat:@"TapMode: %i",self.tapMode]);
+
+    [self.networkManager sendMetatoneMessage:METATONE_RESET_MESSAGE withState:@"reset"];
+    [self.networkManager sendMetatoneMessage:METATONE_TAPMODE_MESSAGE
+                                   withState:[NSString stringWithFormat:@"%d",self.tapMode]];
+    
+    if (arc4random_uniform(100)>75) [self randomiseScale];
 }
+
+// Scale Button
+- (IBAction)changeScale:(UIButton *)sender {
+    [self randomiseScale];
+//    // increment scale mode
+//    self.scaleMode = (self.scaleMode + 1) % 3;
+//    // update scale label
+//    if (self.scaleMode == SCALE_MODE_F_MIXO) [self.scaleLabel setText:@"F Mixo"];
+//    if (self.scaleMode == SCALE_MODE_C_LYDSHARP) [self.scaleLabel setText:@"C Lyd Sharp 5"];
+//    if (self.scaleMode == SCALE_MODE_FSHARP_LYD) [self.scaleLabel setText:@"F# Lydian"];
+//    
+//    [self.networkManager sendMetatoneMessage:METATONE_SCALE_MESSAGE withState:[NSString stringWithFormat:@"%d", self.scaleMode]];
+}
+
+// Scale Method
+- (void)randomiseScale {
+    // increment scale mode
+    self.scaleMode = (self.scaleMode + 1) % 3;
+    // update scale label
+    if (self.scaleMode == SCALE_MODE_F_MIXO) [self.scaleLabel setText:@"F Mixo"];
+    if (self.scaleMode == SCALE_MODE_C_LYDSHARP) [self.scaleLabel setText:@"C Lyd Sharp 5"];
+    if (self.scaleMode == SCALE_MODE_FSHARP_LYD) [self.scaleLabel setText:@"F# Lydian"];
+    
+    [self.networkManager sendMetatoneMessage:METATONE_SCALE_MESSAGE withState:[NSString stringWithFormat:@"%d", self.scaleMode]];
+}
+
 
 - (IBAction)panGestureRecognized:(UIPanGestureRecognizer *)sender {
     CGFloat xVelocity = [sender velocityInView:self.view].x;
     CGFloat yVelocity = [sender velocityInView:self.view].y;
     CGFloat velocity = sqrt((xVelocity * xVelocity) + (yVelocity * yVelocity));
-    
-    //NSLog([NSString stringWithFormat:@"Vel: %f",velocity]);
-    
+        
     if ([sender state] == UIGestureRecognizerStateBegan) {
         // send pan began message
-        [PdBase sendFloat:velocity toReceiver:@"panstarted"];
-        //if (self.oscLogging) [self.networkManager sendMessageWithTouch:[sender locationInView:self.view] Velocity:velocity];
-        
+        [PdBase sendFloat:velocity toReceiver:@"panstarted"];        
     } else if ([sender state] == UIGestureRecognizerStateChanged) {
         // send normal pan changed message
         [PdBase sendFloat:velocity toReceiver:@"touchvelocity" ];
-        
-        //if (self.oscLogging) [self.networkManager sendMessageWithTouch:[sender locationInView:self.view] Velocity:velocity];
-        
     } else if (([sender state] == UIGestureRecognizerStateEnded) || ([sender state] == UIGestureRecognizerStateCancelled)) {
         [PdBase sendBangToReceiver:@"touchended" ];
-        //if (self.oscLogging) [self.networkManager sendMessageTouchEnded];
     }
 }
 
@@ -305,21 +372,21 @@
 {
     // Search network for metatoneLogging sessions
     // Initialise Network
-    self.networkManager = [[MetatoneNetworkManager alloc] initWithDelegate:self];
+    self.networkManager = [[MetatoneNetworkManager alloc] initWithDelegate:self shouldOscLog:self.oscLogging];
     
     if (!self.networkManager) {
         self.oscLogging = NO;
         [self.oscLoggingLabel setText:@"OSC Logging: Not Connected"];
         NSLog(@"OSC Logging: Not Connected");
-    } else {
-        self.oscLogging = YES;
     }
 }
 
 - (void) searchingForLoggingServer {
-    // Spin the spinner - write "Searching for Logging Server" in the field
-    [self.oscLoggingSpinner startAnimating];
-    [self.oscLoggingLabel setText:@"Searching for Logging Server..."];
+    if (self.oscLogging) {
+        // Spin the spinner - write "Searching for Logging Server" in the field
+        [self.oscLoggingSpinner startAnimating];
+        [self.oscLoggingLabel setText:@""];
+    }
 }
 
 -(void) loggingServerFoundWithAddress:(NSString *)address andPort:(int)port andHostname:(NSString *)hostname {
@@ -329,9 +396,65 @@
 }
 
 -(void) stoppedSearchingForLoggingServer {
-    // stop the spinner - write "Logging Server Not Found" in the field.
-    [self.oscLoggingSpinner stopAnimating];
-    [self.oscLoggingLabel setText: @"Logging Server Not Found!"];
+    if (self.oscLogging) {
+        // stop the spinner - write "Logging Server Not Found" in the field.
+        [self.oscLoggingSpinner stopAnimating];
+        [self.oscLoggingLabel setText: @"Logging Server Not Found!"];
+    }
+}
+
+-(void) didReceiveMetatoneMessageFrom:(NSString *)device withName:(NSString *)name andState:(NSString *)state {
+    //NSLog([NSString stringWithFormat:@"METATONE: Received app message from:%@ with state:%@",device,state]);
+    
+    if ([name isEqualToString:METATONE_SCALE_MESSAGE]) {
+        NSLog(@"METATONE: Scale Message received.");
+        self.scaleMode = [state intValue];
+        // update scale label
+        if (self.scaleMode == SCALE_MODE_F_MIXO) [self.scaleLabel setText:@"F Mixo"];
+        if (self.scaleMode == SCALE_MODE_C_LYDSHARP) [self.scaleLabel setText:@"C Lyd Sharp 5"];
+        if (self.scaleMode == SCALE_MODE_FSHARP_LYD) [self.scaleLabel setText:@"F# Lydian"];
+        
+    } else if ([name isEqualToString:METATONE_RESET_MESSAGE]) {
+        NSLog(@"METATONE: Reset Message received.");
+        if (arc4random_uniform(100) > 80) [PdBase sendBangToReceiver:@"randomiseSounds"];
+        
+    } else if ([name isEqualToString:METATONE_TAPMODE_MESSAGE]) {
+        NSLog(@"METATONE: TapMode Message received.");
+        if (arc4random_uniform(100) > 80) self.tapMode = [state intValue];
+    } else if ([name isEqualToString:METATONE_LOOP_MESSAGE]) {
+        if (arc4random_uniform(100)>75) {
+            NSLog(@"METATONE: Loop Message Received and Actioned.");
+            self.tapLooping = YES;
+            [self.loopSwitch setOn:YES animated:YES];
+        }
+    }
+}
+
+-(void)didReceiveGestureMessageFor:(NSString *)device withClass:(NSString *)class {
+    if ([class isEqualToString:self.lastGesture] && arc4random_uniform(100)>85) {
+        if (arc4random_uniform(10)>5) {
+            [self.loopSwitch setOn:!self.loopSwitch.on animated:YES];
+            [self loopingOn:self.loopSwitch];
+        } else {
+            [self.fieldSwitch setOn:!self.fieldSwitch.on animated:YES];
+            [self fieldsOn:self.fieldSwitch];
+        }
+    }
+    self.lastGesture = class;
+    NSLog(@"Gesture: %@",class);
+}
+
+-(void)didReceiveEnsembleState:(NSString *)state withSpread:(NSNumber *)spread withRatio:(NSNumber *)ratio {
+    
+}
+
+-(void)didReceiveEnsembleEvent:(NSString *)event forDevice:(NSString *)device withMeasure:(NSNumber *)measure {
+    if (arc4random_uniform(100)>75) {
+        [self reset:nil];
+        NSLog(@"Received Ensemble Event and Changed!");
+    } else {
+        NSLog(@"Received Ensemble Event and did nothing.");
+    }
 }
 
 @end
