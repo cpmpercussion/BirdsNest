2ac80f4c00aa28741ce57639b144bca5dfd41e9a
Charles Martin <cpm@charlesmartin.com.au>
2017-12-06 10:15:19 +0100

    updated libpd

--- Pods/libpd/libpd_wrapper/z_libpd.c
+++ Pods/libpd/libpd_wrapper/z_libpd.c
@@ -33,6 +33,7 @@
 # define SCHED_TICK(x) sched_tick()
 #endif
 
+// forward declares
 void pd_init(void);
 int sys_startgui(const char *libdir);
 void sys_stopgui(void);
@@ -59,7 +60,7 @@ static void *get_object(const char *s) {
   return x;
 }
 
-/* this is called instead of sys_main() to start things */
+// this is called instead of sys_main() to start things
 int libpd_init(void) {
   static int initialized = 0;
   if (initialized) return -1; // only allow init once (for now)
@@ -121,7 +122,9 @@ void libpd_add_to_search_path(const char *s) {
 void *libpd_openfile(const char *basename, const char *dirname) {
   void * retval;
   sys_lock();
+  pd_globallock();
   retval = (void *)glob_evalfile(NULL, gensym(basename), gensym(dirname));
+  pd_globalunlock();
   sys_unlock();
   return retval;
 }
@@ -555,7 +558,7 @@ void libpd_set_midibytehook(const t_libpd_midibytehook hook) {
   libpd_midibytehook = hook;
 }
 
-int libpd_startgui(char *path) {
+int libpd_start_gui(char *path) {
   int retval;
   sys_lock();
   retval = sys_startgui(path);
@@ -563,22 +566,63 @@ int libpd_startgui(char *path) {
   return retval;
 }
 
-void libpd_stopgui( void) {
+void libpd_stop_gui(void) {
   sys_lock();
   sys_stopgui();
   sys_unlock();
 }
 
-void libpd_pollgui( void) {
+void libpd_poll_gui(void) {
   sys_lock();
   sys_pollgui();
   sys_unlock();
 }
 
-/* dummy routines needed because we don't use s_file.c */
+t_pdinstance *libpd_new_instance(void) {
+#ifdef PDINSTANCE
+  return pdinstance_new();
+#else
+  return 0;
+#endif
+}
+
+void libpd_set_instance(t_pdinstance *x) {
+#ifdef PDINSTANCE
+  pd_setinstance(x);
+#endif
+}
+
+void libpd_free_instance(t_pdinstance *x) {
+#ifdef PDINSTANCE
+  pdinstance_free(x);
+#endif
+}
+
+t_pdinstance *libpd_this_instance(void) {
+  return pd_this;
+}
+
+t_pdinstance *libpd_get_instance(int index) {
+#ifdef PDINSTANCE
+  if(index < 0 || index >= pd_ninstances) {return 0;}
+  return pd_instances[index];
+#else
+  return pd_this;
+#endif
+}
+
+int libpd_num_instances(void) {
+#ifdef PDINSTANCE
+  return pd_ninstances;
+#else
+  return 1;
+#endif
+}
+
+// dummy routines needed because we don't use s_file.c
 void glob_loadpreferences(t_pd *dummy, t_symbol *s) {}
 void glob_savepreferences(t_pd *dummy, t_symbol *s) {}
 void glob_forgetpreferences(t_pd *dummy) {}
 void sys_loadpreferences(const char *filename, int startingup) {}
 int sys_oktoloadfiles(int done) {return 1;}
-void sys_savepreferences(const char *filename) {} /* used in s_path.c */
+void sys_savepreferences(const char *filename) {} // used in s_path.c
